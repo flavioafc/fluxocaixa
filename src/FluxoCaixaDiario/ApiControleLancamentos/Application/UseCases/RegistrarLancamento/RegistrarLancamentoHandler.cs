e470380644bf21a81c232c62a0b00e5aa633628d
Flavio Costa <flavio.costa@vidi.com>
2025-02-01 10:21:13 -0300

    Log implementado no handler de registro de lançamento

--- src/FluxoCaixaDiario/ApiControleLancamentos/Application/UseCases/RegistrarLancamento/RegistrarLancamentoHandler.cs
+++ src/FluxoCaixaDiario/ApiControleLancamentos/Application/UseCases/RegistrarLancamento/RegistrarLancamentoHandler.cs
@@ -12,19 +12,25 @@ namespace ApiControleLancamentos.Application.UseCases.RegistrarLancamento
         private readonly ILancamentoRepository _repository;
         private readonly IEventPublisher _eventPublisher;
         private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<RegistrarLancamentoHandler> _logger;
 
         public RegistrarLancamentoHandler(
             ILancamentoRepository repository,
             IEventPublisher eventPublisher,
-            IUnitOfWork unitOfWork)
+            IUnitOfWork unitOfWork,
+            ILogger<RegistrarLancamentoHandler> logger)
         {
             _repository = repository;
             _eventPublisher = eventPublisher;
             _unitOfWork = unitOfWork;
+            _logger = logger;
         }
 
         public async Task<int> Handle(RegistrarLancamentoCommand command)
         {
+            var correlationId = Guid.NewGuid().ToString();
+            _logger.LogInformation("Iniciando processamento do lançamento", new { Dados = command, Timestamp = DateTime.UtcNow, CorrelationId = correlationId });
+
             // Cria o lançamento
             var lancamento = new Lancamento(command.Valor, command.Tipo, command.Descricao, DateTime.UtcNow);
 
@@ -40,6 +46,7 @@ namespace ApiControleLancamentos.Application.UseCases.RegistrarLancamento
             {
                 // Publica o evento e tenta novamente em caso de falhas transitórias
                 await PollyPolicies.RetryPolicy.ExecuteAsync(() => _eventPublisher.PublishAsync(evento));
+                _logger.LogInformation("Evento publicado com sucesso", new { Evento = evento, CorrelationId = correlationId });
 
                 // Confirma as mudanças no banco apenas após publicação bem-sucedida
                 await _unitOfWork.CommitAsync();
@@ -47,10 +54,12 @@ namespace ApiControleLancamentos.Application.UseCases.RegistrarLancamento
             catch (Exception ex)
             {
                 // Loga a falha e pode lançar para o consumidor final, caso necessário
+                _logger.LogError(ex, "Falha ao publicar o evento no RabbitMQ", new { CorrelationId = correlationId });
                 await _unitOfWork.RollbackAsync();
                 throw new InvalidOperationException("Falha ao publicar o evento no RabbitMQ", ex);
             }
 
+            _logger.LogInformation("Lançamento registrado com sucesso", new { LancamentoId = lancamento.Id, CorrelationId = correlationId });
             return lancamento.Id;
         }
     }
